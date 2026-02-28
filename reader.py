75e1a8c27a3722b99f374b6277041f8d8ef3a912
Astrid Lauenstein <me@acqrel.com>
2026-02-28 11:35:19 +0100

    copy for real this time

--- reader.py
+++ reader.py
@@ -1,43 +1,73 @@
-import serial
 import time
+import threading
+import queue
+
+import serial
 import numpy as np
-from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 
-hist_size = 1000
-data = np.zeros(hist_size)
-X = np.arange(hist_size)
+DATA_QUEUE = queue.Queue(maxsize=10)
 
-graph = plt.plot(X, data)[0]
 
-mid = 256 / 2
+def read_thread():
+    print("reading")
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 
-was_high = False
-while True:
-    start = time.time()
-    switches = 0
-    for i in range(hist_size):
-        # Read single byte
-        byte = ser.read(1)
+    buffer_size = 1000
+    last_t = time.time()
+    while True:
+        data = b""
+        while len(data) < buffer_size:
+            data += ser.read(buffer_size - len(data))
 
-        data[i] = byte[0]
-        if byte[0] > mid and not was_high:
-            switches += 1
-            was_high = True
-        else:
-            was_high = False
+        now = time.time()
+        dt = (now - last_t) / len(data)
+        last_t = now
 
-        # if byte:
-        #     print(f"Got byte: {byte[0]:02X}")
-        # else:
-        #     print("nothing to read")
-    graph.set_ydata(data)
-    plt.ylim([0, 256])
-    plt.draw()
-    plt.pause(1e-3)
-    end = time.time()
-    sampling_speed = 1 / (end - start) / 1000 * hist_size
-    print(
-        f"freq: {switches / hist_size} sampling: {sampling_speed:.2f} khz")
+        data = np.array(list(data)) / 255
+        try:
+            DATA_QUEUE.put_nowait((data, dt))
+            print(f"receiving @ {1.0/dt/1000:6.3f} kHz")
+        except queue.Full:
+            print("dropping data")
+
+
+def main():
+    graph = plt.plot([0], [0])[0]
+
+    while True:
+        data, dt = DATA_QUEUE.get()
+
+        y = data - np.mean(data)
+
+        # Find the positions of the rising edge
+        next_y = np.concat([y[1:], y[-1:]])
+        edge = (next_y > 0) > (y > 0) # beautiful code
+        has_edge = np.where(edge)[0]
+
+        # Interpolate and scale to find exact edge positions
+        edge_pos = has_edge + y[has_edge] / (y[has_edge] - next_y[has_edge])
+        edge_pos = edge_pos * dt
+
+        deltas = edge_pos[1:] - edge_pos[:-1]
+
+        x2 = edge_pos[:-1]
+        y2 = 1.0 / deltas
+
+        graph.set_data(x2, y2)
+
+        plt.xlim([0, 1000 / 10_000]) # 1000 samples @ 10 kHz
+        plt.ylim([0, 1400])
+
+        plt.draw()
+        plt.pause(1e-3)
+
+
+if __name__ == "__main__":
+    threading.Thread(target=read_thread, daemon=True).start()
+
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
