6c746eef6ed22672b62c409677525a816c8066f6
Jonas <jonas.bonnaudet@gmail.com>
2026-02-28 19:41:40 +0100

    lol

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@ done
 
 cd /home/jonas/image_sender_3000
 git pull
-echo 123 | sudo -S nmcli device wifi connect Albinisme password 135792468
+echo 123 | sudo -S nmcli device wifi connect Albinisme
 python /home/jonas/image_sender_3000/send.py &> /home/jonas/log.txt
