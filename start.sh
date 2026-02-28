c9a32aec61ab30f2e95cb337ff3af38dd3de1b8e
Jonas <jonas.bonnaudet@gmail.com>
2026-02-28 11:21:23 +0100

    internet stuff

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
+until curl -s --head https://www.google.com > /dev/null; do
+    echo "No internet yet..."
+    sleep 5
+done
+
 cd /home/jonas/image_sender_3000
 git pull
 python /home/jonas/image_sender_3000/send.py
